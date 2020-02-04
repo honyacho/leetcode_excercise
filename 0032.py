9abe72ec8156fbbc1786c4cd6a53bf4ff79049b0
Tatsuki Saito <cc_called_pizza@yahoo.co.jp>
2020-02-04 10:41:07 +0900

    update

---
+++ 0032.py
@@ -0,0 +1,24 @@
+class Solution:
+    def __init__(self):
+        self.maxvalue = 0
+
+    def longestValidParentheses(self, s: str) -> int:
+        res = self.solve(s, 0)
+        return self.maxvalue
+
+    def solve(self, s, depth):
+        res = 0
+        while s[res:]:
+            if s[res] == '(':
+                res += 1
+                res += self.solve(s[res:], depth + 1)
+                res += 1
+                if res-1 < len(s) and s[res-1] == ')':
+                    self.maxvalue = max(self.maxvalue, res)
+            else:
+                if depth == 0:
+                    s = s[res+1:]
+                    res = 0
+                else:
+                    return res
+        return res
---
+++ 0033.py
@@ -0,0 +1,26 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums: return -1
+        lo, hi = 0, len(nums)-1
+        L = len(nums)
+        while lo != hi:
+            mid = (lo+hi)//2
+            if nums[mid] < nums[hi]:
+                hi = mid
+            else:
+                lo = mid+1
+        zero = lo
+        lo, hi = 0, len(nums)-1
+
+        while lo != hi:
+            mid = (lo+hi)//2
+            if target > nums[(mid+zero)%L]:
+                lo = mid+1
+            else:
+                hi = mid
+        # print(zero, lo)
+        
+        if nums[(lo+zero)%L] == target:
+            return (lo+zero)%L
+        else:
+            return -1
